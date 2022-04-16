12588a57048b1c005e469187cf758afa05ef4f68
Sam <shung93@gmail.com>
2022-04-16 09:21:34 +0100

    Store user data in user area in db

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import {connect} from 'react-redux'; 
-import {Route, Redirect}  from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
 export const PrivateRoute = ({
-    isAuthenticated, 
-    component: Component,
-    ...restOfProps
+  isAuthenticated,
+  component: Component,
+  ...rest
 }) => (
-    <Route {...restOfProps} component={(props) => (
-        isAuthenticated ? (
-            <div>
-                <Header/>
-                <Component {...props} />
-            </div>
-        ) : (
-            <Redirect to="/" />
+    <Route {...rest} component={(props) => (
+      isAuthenticated ? (
+        <div>
+          <Header />
+          <Component {...props} />
+        </div>
+      ) : (
+          <Redirect to="/" />
         )
     )} />
-);
+  );
 
 const mapStateToProps = (state) => ({
-    isAuthenticated: !!state.auth.uid // boolean true if authenticated
+  isAuthenticated: !!state.auth.uid
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
